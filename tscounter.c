37fad340443e70cbb0f463eb8f367ec3116568f3
1-seungho <83808255+1-seungho@users.noreply.github.com>
2021-06-12 15:44:04 +0900

    Update tscounter.c

--- tscounter.c
+++ tscounter.c
@@ -3,10 +3,23 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/type.h>
+#include <time.h>
+
+union semun {
+        int val
+        struct semid_ds *buf
+        ushort *array;
+};
+
+#define PATH "mnt/C/Users/Han/Desktop
 
 typedef struct __counter_t {
     int value;
-    pthread_mutex_t lock;
+    key_t key;
+    struct sembuf s;
 } counter_t;
 
 unsigned int loop_cnt;
@@ -14,25 +27,58 @@ counter_t counter;
 
 void init(counter_t *c) {
     c->value = 0;
-    pthread_mutex_init(&c->lock, NULL);
+    key_t key;
+    key = fhok(PATH, 'z');
+    c->semid = semget(c->key, 1,0600 | IPC_CREAT);
 }
 
 void increment(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
+     int semid;
+    struct sembuf s;
+    
+    s.sem_num = 0;
+    s.sem_op = -1; 
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
     c->value++;
-    pthread_mutex_unlock(&c->lock);
+    
+    s.sem_num = 0;
+    s.sem_op = 1; 
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
 }
 
 void decrement(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
+    int semid;
+    struct sembuf s;
+    
+    s.sem_num = 0;
+    s.sem_op = -1; 
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
     c->value--;
-    pthread_mutex_unlock(&c->lock);
+    
+    s.sem_num = 0;
+    s.sem_op = 1; 
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
 }
 
 int get(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
+     int semid;
+    struct sembuf s;
+    
+    s.sem_num = 0;
+    s.sem_op = -1; 
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
+    
     int rc = c->value;
-    pthread_mutex_unlock(&c->lock);
+    
+    s.sem_num = 0;
+    s.sem_op = 1; 
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
     return rc;
 }
 
@@ -51,8 +97,11 @@ void *mythread(void *arg)
                                                                              
 int main(int argc, char *argv[])
 {                    
+    clock_t start, end;
+    float time;
+    start = clock();
+    
     loop_cnt = atoi(argv[1]);
-
     init(&counter);
 
     pthread_t p1, p2;
@@ -63,5 +112,9 @@ int main(int argc, char *argv[])
     pthread_join(p1, NULL); 
     pthread_join(p2, NULL); 
     printf("main: done [counter: %d] [should be: %d]\n", get(&counter), loop_cnt * 2);
+    
+    finish = clock();
+    time = (float)(finish-start)/CLOCKS_PER_SEC;
+    printf("Semaphore Performance Time : %.3f \n", time);
     return 0;
 }
